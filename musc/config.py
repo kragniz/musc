d1b7a4ad1eb2b2bdafa1232ad9d9e9c94f5ae904
Louis Taylor <kragniz@gmail.com>
2012-07-08 02:27:52 +0100

    Some pretty rancid code to get scrobbling working.
    And it does!

--- musc/config.py
+++ musc/config.py
@@ -1,6 +1,10 @@
+import os
+
 PORT=6674
 HOST='localhost'
 debug=False
+
+sessionFile = os.path.join(os.path.expanduser('~'), '.musc')
 
 apiKey = '9db5c55535ef08c1d6be806642a5167c'
 apiSig = '4b21539ea4d9bed22cbc1ce768f55ed4'
--- musc/scrobble.py
+++ musc/scrobble.py
@@ -2,14 +2,25 @@ import urllib
 import httplib
 import re
 import config
+import os
+import hashlib
+import time
 
 class Scrobbler(object):
     def __init__(self):
-        self.token = self.getToken
+        self.token = self.getToken()
+
+    def sessionKey(self):
+        if os.path.exists(config.sessionFile):
+            with open(config.sessionFile) as f:
+                return f.read()
+        else:
+            self.logIn()
+            return self.getSession()
 
     def request(self, args):
         params = urllib.urlencode(args)
-        header = {'user-agent' : 'musc/0.1',
+        header = {'user-agent': 'musc/0.1',
                   'Content-type': 'application/x-www-form-urlencoded'}
 
         lastfm = httplib.HTTPConnection('ws.audioscrobbler.com')
@@ -17,12 +28,52 @@ class Scrobbler(object):
         response = lastfm.getresponse()
         return response.read()
 
+    def logIn(self):
+        url = 'http://www.last.fm/api/auth/?api_key={0}&token={1}'.format(
+                                                     config.apiKey,
+                                                     self.token)
+        print 'Please log in at this url then press enter:', url
+        raw_input()
+
+    def getSession(self):
+        params = {'method': 'auth.getSession',
+                  'api_key': config.apiKey,
+                  'token': self.token}
+        params['api_sig'] = self.sign(params)
+        sessionKey = self.request(params)
+        sessionKey = re.search('<key.+key>', sessionKey).group()[5:-6]
+        with open(config.sessionFile, 'w') as f:
+            f.write(sessionKey)
+        return sessionKey
+
     def getToken(self):
-        token = self.request({'method' : 'auth.getToken',
-                              'api_sig' : config.apiSig,
-                              'api_key' : config.apiKey})
+        token = self.request({'method': 'auth.getToken',
+                              'api_sig': config.apiSig,
+                              'api_key': config.apiKey})
         return re.search('<token.+token>', token).group()[7:-8]
+
+    def scrobble(self, artist, track):
+        timestamp = str(int(time.time()))
+        params = {'artist': artist,
+                  'method': 'track.scrobble',
+                  'track': track,
+                  'timestamp': timestamp,
+                  'api_key': config.apiKey,
+                  'sk': self.sessionKey()}
+        params['api_sig'] = self.sign(params)
+        print self.request(params)
+
+    def sign(self, methods):
+        l = []
+        s = ''
+        for k, v in methods.items():
+            l += [k+v]
+        l.sort()
+        for i in l:
+            s += i
+        return hashlib.md5(s+config.apiSig).hexdigest()
+
 
 if __name__ == '__main__':
     s = Scrobbler()
-    print s.getToken()
+    s.scrobble('Parov Stelar', 'For Rose')
